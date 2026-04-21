7d2cb5bc63ceb68bb21434d9a384919d24b540b7
Neema <patelneema93@gmail.com>
2026-04-21 16:20:48 -0400

    add some check lengh check and find word example

--- string.py
+++ string.py
@@ -1,11 +1,24 @@
 #inside quote
 print("Hello")
+
+
 #singleline varible
 a = "Hello"
 print(a)
+
+
 #mulitiline variable
 b = """Hello
 This is Neema.
 My age is 32.
 """ 
 print(b)
+
+
+# find length of string
+x = "Hello, World!"
+print(len(x))
+
+#check string
+txt = "The best things in life are free!"
+print("free" in txt)
