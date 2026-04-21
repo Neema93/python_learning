1aa9d0f765187ced2507b447a6522b586492c121
Neema <patelneema93@gmail.com>
2026-04-21 16:25:48 -0400

    add example for slicing string

--- string.py
+++ string.py
@@ -22,3 +22,19 @@ print(len(x))
 #check string
 txt = "The best things in life are free!"
 print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+#check if not
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+#Slicing Strings
+b = "Hello, World!"
+print(b[2:5])
