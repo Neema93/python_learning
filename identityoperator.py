fedf819329bf067e02d5e9a7bb2b8c2aa34f1e5d
Neema <patelneema93@gmail.com>
2026-04-23 09:55:25 -0400

    create identityoperator

---
+++ identityoperator.py
@@ -0,0 +1,10 @@
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
+print(x is not y)
+# is - Checks if both variables point to the same object in memory
+# == - Checks if the values of both variables are equal
