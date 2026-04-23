5ffbba78db40f36e0d7b155a377088198902b21c
Neema <patelneema93@gmail.com>
2026-04-23 14:36:38 -0400

    create tuple

---
+++ tuple.py
@@ -0,0 +1,18 @@
+"""Tuple
+Tuples are used to store multiple items in a single variable.
+
+Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+
+A tuple is a collection which is ordered and unchangeable.
+
+Tuples are written with round brackets."""
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+"""Tuple Methods
+Python has two built-in methods that you can use on tuples.
+
+Method	Description
+count()	Returns the number of times a specified value occurs in a tuple
+index()	Searches the tuple for a specified value and returns the position of where it was found"""
