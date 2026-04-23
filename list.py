90f1b511faa7334842862f622d45ac011579f1bf
Neema <patelneema93@gmail.com>
2026-04-23 13:39:43 -0400

    add all list method list

--- list.py
+++ list.py
@@ -27,3 +27,34 @@ print(thislist)
 # Clear the List
 thislist.clear()
 print(thislist)
+# Loop Through a List
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+# Loop Through the Index Numbers
+for i in range(len(thislist)):
+  print(i)
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+# Using a While Loop
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+""" List Methods
+Python has a set of built-in methods that you can use on lists.
+
+Method	Description
+append()	Adds an element at the end of the list
+clear()	Removes all the elements from the list
+copy()	Returns a copy of the list
+count()	Returns the number of elements with the specified value
+extend()	Add the elements of a list (or any iterable), to the end of the current list
+index()	Returns the index of the first element with the specified value
+insert()	Adds an element at the specified position
+pop()	Removes the element at the specified position
+remove()	Removes the item with the specified value
+reverse()	Reverses the order of the list
+sort()	Sorts the list
+"""
