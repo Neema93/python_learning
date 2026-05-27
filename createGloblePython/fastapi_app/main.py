123bd2213ba21809555651f61f1c09a99ab6a549
Neema <patelneema93@gmail.com>
2026-05-27 14:30:10 -0400

    create get and post task

--- createGloblePython/fastapi_app/main.py
+++ createGloblePython/fastapi_app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 #fackdatabase
-task = []
-
+tasks = []
+class Task(BaseModel): # type: ignore
+    title: str
 @app.get("/")
 def home():
     return{"message":"Fastapi is running"}
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.post("/tasks")
+def add_task(task: Task):
+    tasks.append(task)
+    return {
+        "message": "Task added",
+        "task": task
+    }
