bb7f444488b92d3a266c9891547db405af8ddac6
Neema <patelneema93@gmail.com>
2026-05-27 15:28:46 -0400

    delete task by id

--- createGloblePython/fastapi_app/main.py
+++ createGloblePython/fastapi_app/main.py
@@ -6,14 +6,16 @@ tasks = []
 class Task(BaseModel): 
     title: str
     completed: bool = False
-
+# home get method
 @app.get("/")
 def home():
     return{"message":"Fastapi is running"}
-
+#get all task
 @app.get("/tasks")
 def get_tasks():
     return tasks
+
+#get task by id
 @app.get("/tasks/{task_id}")
 def get_taskid(task_id:int):
        
@@ -21,7 +23,7 @@ def get_taskid(task_id:int):
         return tasks[task_id]
 
     return {"error": "Task not found"}
-
+# add task
 @app.post("/tasks")
 def add_task(task: Task):
     tasks.append(task)
@@ -39,6 +41,19 @@ def update_task(task_id: int, updated_task: Task):
         return {
             "message": "Task updated",
            "updated_task": tasks[task_id]
+        }
+
+    return {"error": "Task not found"}
+#delete task
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+
+    if 0 <= task_id < len(tasks):
+        removed_task = tasks.pop(task_id)
+
+        return {
+            "message": "Task deleted successfully",
+            "deleted_task": removed_task
         }
 
     return {"error": "Task not found"}
